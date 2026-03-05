ca9df9307540b90015cfbbf46f89d11aad2c2a4f
jacobsala <jsala@udel.edu>
2026-03-05 15:18:07 -0500

    made a funciton that combined all that I've reviewed

--- countTruthy.ts
+++ countTruthy.ts
@@ -7,3 +7,42 @@ export function countTruthy(values: Value[]): number {
     }
     return count;
 }
+
+export function sumPassing(grades: Grade[], passingGrade: number): number {
+    let sum: number = 0;
+    for (let grade of grades) {
+        if (grade >= passingGrade) {
+            sum += grade;
+        }
+    }
+    return sum;
+}
+
+export function getIndoorCats(cats: Cat[]): Cat[] {
+    let result: Cat[] = [];
+    for(let cat of cats) {
+        if(!cat.isOutdoor) {
+            result.push(cat);
+        }
+    }
+    return result;
+}
+
+export function boostGrades(grades: number[]): void {
+    for (let i = 0; i<grades.length; i++) {
+        if(grades[i] === 50) {
+            grades[i] = 70;
+        }
+    }
+}
+
+export function getBigBags(bags: Bag[]): Bag[] {
+    let result: Bag[] = [];
+    for(let bag of bags) {
+        if(bag.width > 5) {
+            let deepCopy = new Bag(bag.width, bag.height);
+            result.push(deepCopy);
+        }
+    }
+    return result;
+}
